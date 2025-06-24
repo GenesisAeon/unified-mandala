28ac572133b37b86f6946f76445b154fe7b256cb
GenesisAeon <joberoemer@gmail.com>
2025-06-24 18:58:57 +0200

    Add offline docker package

---
+++ localhost-offline/services/offline-gpt/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const responses = require('./model/responses.json');
+app.use(express.json());
+
+app.post('/chat', (req, res) => {
+  const prompt = req.body.prompt;
+  const answer = responses[prompt] || 'Ich kenne keine Antwort darauf.';
+  res.json({ reply: answer });
+});
+
+app.listen(5000, () => console.log('Offline GPT Mock running')); 
