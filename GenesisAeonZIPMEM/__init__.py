76937d76a55faafb515914c9e5c4b91bc49736fc
GenesisAeon <joberoemer@gmail.com>
2025-06-26 23:22:13 +0200

    feat(zipmem): add AeonSealAI integrated system

--- GenesisAeonZIPMEM/__init__.py
+++ GenesisAeonZIPMEM/__init__.py
@@ -6,6 +6,7 @@ from .dynamic_task_allocator import DynamicTaskAllocator
 from .fractal_agent import FractalAgent
 from .master_coordinator import MasterCoordinator
 from .advanced_ai_system import AdvancedAISelfLearnSystem
+from .aeon_seal_ai import AeonSealAI
 from .conversation_importer import load_conversation_fragments, import_into_memory
 
 __all__ = [
@@ -15,6 +16,7 @@ __all__ = [
     "FractalAgent",
     "MasterCoordinator",
     "AdvancedAISelfLearnSystem",
+    "AeonSealAI",
     "load_conversation_fragments",
     "import_into_memory",
 ]
