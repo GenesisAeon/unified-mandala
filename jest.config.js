789607eb766b4082cada7d755e83cb98065037ce
GenesisAeon <joberoemer@gmail.com>
2025-07-02 11:57:07 +0200

    feat: update tooling and docs

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   transform: { '^.+\\.tsx?$': ['<rootDir>/node_modules/ts-jest', {}] },
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/dist/'],
+  testPathIgnorePatterns: ['/dist/', 'packages/agents/__tests__', 'services/ghost-shell/server.test.ts'],
   moduleNameMapper: {
     '^yaml$': '<rootDir>/node_modules/yaml/dist/index.js'
   },
+  collectCoverage: true,
+  coverageReporters: ['text', 'lcov'],
   detectOpenHandles: true
 };
