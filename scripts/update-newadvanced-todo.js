66870eb7f8a316daf3902fbd8043b0cdec06d863
GenesisAeon <joberoemer@gmail.com>
2025-07-03 11:24:23 +0200

    feat: update advanced ToDos from new conversations

---
+++ scripts/update-newadvanced-todo.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const path = require('path');
+let { updateAdvancedTodo } = require('../packages/shared-utils/advancedTodoUpdater.js');
+const { updateProgress } = require('./update-advancedprogress.js');
+
+const convFile = path.join(__dirname, '../docs/sigils/newadvancedconversations.json');
+const yamlFile = path.join(__dirname, '../advancedToDo.yaml');
+const jsonFile = path.join(__dirname, '../advancedToDo.json');
+
+const entries = updateAdvancedTodo(convFile, yamlFile, jsonFile);
+console.log(`advancedToDo updated with ${entries.length} tasks from newadvancedconversations.json.`);
+updateProgress('sync');
