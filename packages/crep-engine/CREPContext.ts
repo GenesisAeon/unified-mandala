42e79d7c6317f757e641d1a4148e9e4ed7c8c40a
GenesisAeon <joberoemer@gmail.com>
2025-07-09 14:21:04 +0200

    chore(progress): update progress state

---
+++ packages/crep-engine/CREPContext.ts
@@ -0,0 +1,39 @@
+import { EventEmitter } from 'events';
+import { CREPEntry } from './types';
+
+export interface CREPState {
+  history: CREPEntry[];
+}
+
+export type CREPAction =
+  | { type: 'ADD'; entry: CREPEntry }
+  | { type: 'RESET' };
+
+export class CREPContext extends EventEmitter {
+  private state: CREPState;
+
+  constructor(initial: CREPState = { history: [] }) {
+    super();
+    this.state = initial;
+  }
+
+  dispatch(action: CREPAction) {
+    switch (action.type) {
+      case 'ADD':
+        this.state.history.push(action.entry);
+        this.emit('update', this.state);
+        break;
+      case 'RESET':
+        this.state.history = [];
+        this.emit('update', this.state);
+        break;
+      default:
+        // ignore
+        break;
+    }
+  }
+
+  getState() {
+    return this.state;
+  }
+}
