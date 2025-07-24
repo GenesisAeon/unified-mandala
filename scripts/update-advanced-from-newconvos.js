fefdedfcc0fd552e9bac730a1de39b68ed793c3f
GenesisAeon <joberoemer@gmail.com>
2025-07-24 20:15:18 +0200

    chore: add script to update advanced todos

---
+++ scripts/update-advanced-from-newconvos.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const { updateAdvancedTodo } = require('../packages/shared-utils/advancedTodoUpdater');
+
+function updateFromNewConvos(convPath = path.join(__dirname, '../docs/sigils/newadvancedconversations.json'),
+                             yamlPath = path.join(__dirname, '../advancedToDo.yaml'),
+                             jsonPath = path.join(__dirname, '../advancedToDo.json')) {
+  updateAdvancedTodo(convPath, yamlPath, jsonPath);
+  console.log(`Updated advancedToDo files from ${path.basename(convPath)}`);
+}
+
+if (require.main === module) {
+  updateFromNewConvos();
+}
+
+module.exports = { updateFromNewConvos };
