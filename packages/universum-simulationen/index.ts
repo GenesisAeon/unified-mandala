49f44a6bdbdd0740693570a910e4563899c46649
GenesisAeon <joberoemer@gmail.com>
2025-08-07 22:46:40 +0200

    feat: add Nullfeld simulation modules

--- packages/universum-simulationen/index.ts
+++ packages/universum-simulationen/index.ts
@@ -4,3 +4,5 @@ export * from './InteractiveNarrativeEngine';
 export * from './SimHostCore';
 export * from './SoundResonanceModule';
 export * from './PyramidNodeSimulation';
+export * from './NullfeldSimulation';
+export * from './NullfeldVisualization';
